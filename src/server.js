0345ee9155e931f391bd81731733cfb52566b015
Natalie Lynch <92595336+NLynch5@users.noreply.github.com>
2022-01-13 16:40:50 +0000

    updated port

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const userRouter = require("./user/user.routes");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
